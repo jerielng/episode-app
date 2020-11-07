603a6d6e4831c74c9bbbf13f79afaf89ad7e240f
Jeriel <jeriel.ng@gmail.com>
2020-11-07 01:06:39 -0500

    Add branded colors

---
+++ lib/constants/episode_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EpisodeColors {
+  static Color black = Color(0xFF000000);
+  static Color episodeBlue = Color(0xFF00C2FF);
+  static Color episodeGreen = Color(0xFF19D989);
+  static Color episodeRed = Color(0xFFEE6C6C);
+  static Color fadedBlack = Color(0xFF363636);
+  static Color gray = Color(0xFFC4C4C4);
+}
--- lib/constants/episode_strings.dart
+++ lib/constants/episode_strings.dart
@@ -1,6 +1,8 @@
 class EpisodeStrings {
-  static String markAllUnwatched = "Mark All Unwatched";
-  static String markAllWatched = "Mark All Watched";
+  static String buttonAddEpisodeToShows = "Add Episode to My Shows";
+  static String buttonFindNewShow = "Find a New Show";
+  static String buttonMarkAllUnwatched = "Mark All Unwatched";
+  static String buttonMarkAllWatched = "Mark All Watched";
   static String titleAddShow = "Add Show";
   static String titleMyShows = "My Shows";
   static String titleWelcomeBack = "Welcome Back!";
